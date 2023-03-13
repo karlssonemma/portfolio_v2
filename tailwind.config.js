d2a484a969261dcdd849193bd309519635567192
emma <karlssonemma93@gmail.com>
2023-03-13 13:01:05 +0100

    tailwind & postcss config updates since the studio was crashing. seems to work now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const path = require('path');
+
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    path.join(__dirname, './app/**/*.{js,ts,jsx,tsx}'),
+    path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+    path.join(__dirname, './components/**/*.{js,ts,jsx,tsx}')
  
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    // './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     fontFamily: {
